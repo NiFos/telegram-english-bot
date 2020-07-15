24ce5461869bcffc7aba729cc18e0bec6c646b21
Vlad <vlad.nifos@gmail.com>
2020-07-15 17:34:52 +0300

    bot.action separately from index.ts

---
+++ src/actions/check.ts
@@ -0,0 +1,12 @@
+import { Telegraf, Context } from 'telegraf';
+import { checkWord } from '../db/controllers/userController';
+
+export async function check(bot: Telegraf<Context>) {
+  bot.action(/^checked-+([A-Za-z]+)/, async (ctx) => {
+    const word = ctx.callbackQuery.data.split(/-/);
+    const response = await checkWord(ctx.from.id, word[1]);
+    if (!response) return null;
+  
+    return ctx.editMessageText(`${word[1]} - checked as learned!`);
+  });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import 'dotenv/config';
-import { Telegraf, Markup } from 'telegraf';
+import { Telegraf } from 'telegraf';
 import { connectMongo } from './db/mongo';
 import { start } from './commands/start';
 import { help } from './commands/help';
 import { list } from './commands/list';
 import { addWord } from './commands/add-word';
 import { learn } from './commands/learn';
-import { checkWord } from './db/controllers/userController';
+import { check } from './actions/check';
 
 connectMongo();
 
@@ -19,15 +19,7 @@ bot.help(help);
 bot.use(addWord);
 list(bot);
 learn(bot);
-
-bot.action(/^checked-+([A-Za-z]+)/, async (ctx) => {
-  const word = ctx.callbackQuery.data.split(/-/);
-  const response = await checkWord(ctx.from.id, word[1]);
-  if (!response) return null;
-
-  return ctx.editMessageText(`${word[1]} - checked as learned!`);
-});
-
+check(bot);
 
 bot.launch();
 console.log('Bot launched!');
